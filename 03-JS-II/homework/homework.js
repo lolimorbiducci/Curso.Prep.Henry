1c12673575c197d0e1a016e9330f364dc598d917
Petro <lmorbiducci@gmail.com>
2022-01-26 04:41:23 -0300

    homework 03 done

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -209,6 +209,7 @@ function tieneTresDigitos(numero){
   }
 }
 
+
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
