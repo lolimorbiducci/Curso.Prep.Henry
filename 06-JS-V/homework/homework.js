eacb0d1ec64e9a38b7277aa694f96a61268fe05f
Petro <lmorbiducci@gmail.com>
2022-02-12 10:51:56 -0300

    homework 06 done

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -88,8 +88,8 @@ function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
-  var nuevaPersona = new Persona (nombre, apellido, edad, dir);
-  return nuevaPersona;
+  var newPerson = new Persona (nombre, apellido, edad, dir);
+  return newPerson;
 }
   
 function agregarMetodo() {
